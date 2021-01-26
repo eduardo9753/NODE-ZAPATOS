f2f8defebfd89904d28dae113ff4893fe4a6c60b
Eduardo <picis_9716@hotmail.com>
2021-01-26 12:19:43 -0500

    Repositorio 4 Zapatos Node-JS

--- src/controllers/zapatos.controller.js
+++ src/controllers/zapatos.controller.js
@@ -29,16 +29,16 @@ zapatoController.formAdd = async (req, res) => {
         console.log('DATA SHOES : ', req.file);
         console.log('DATA USER:', req.user);
         const zapato = new Zapato();
-        zapato.setFoto = req.file.originalname;    //NOMBRE DE LA FOTO 
-        zapato.setMimetype = req.file.mimetype;        //TIPO DE FOTO O IMG
-        zapato.setFilename = req.file.filename;        //NOMBRE ENCRYPTADO
-        zapato.setPath = 'shoes/' + req.file.filename;//CUANDO SE RECORRA LAS IMG BUSCARA ESTA RUTA DEL "PATH"
-        zapato.setSize = req.file.size;            //TAMAÑO DE LA FOTO
-        zapato.setPrecio = req.body.precio;          //CAJA DE TEXTO
-        zapato.setColor = req.body.color;           //CAJA DE TEXTO
-        zapato.setCantidad = req.body.cantidad;        //CAJA DE TEXTO
+        zapato.setFoto      = req.file.originalname;    //NOMBRE DE LA FOTO 
+        zapato.setMimetype  = req.file.mimetype;        //TIPO DE FOTO O IMG
+        zapato.setFilename  = req.file.filename;        //NOMBRE ENCRYPTADO
+        zapato.setPath      = 'shoes/' + req.file.filename;//CUANDO SE RECORRA LAS IMG BUSCARA ESTA RUTA DEL "PATH"
+        zapato.setSize      = req.file.size;            //TAMAÑO DE LA FOTO
+        zapato.setPrecio    = req.body.precio;          //CAJA DE TEXTO
+        zapato.setColor     = req.body.color;           //CAJA DE TEXTO
+        zapato.setCantidad  = req.body.cantidad;        //CAJA DE TEXTO
         zapato.setDBOEstilo = req.body.cboEstilo;       //CAJA DE TEXTO
-        zapato.setDBOTalla = req.body.cboTalla;        //CAJA DE TEXTO
+        zapato.setDBOTalla  = req.body.cboTalla;        //CAJA DE TEXTO
         zapato.setDBOGenero = req.body.cboGenero;       //CAJA DE TEXTO
         zapato.setIdUsuario = req.user.id;              //ID DEL USUARIO LOGEADO
         const correct = await pool.query('INSERT INTO dbozapato(foto,mimetype,filename,path,size,precio,color,cantidad,user_id,dboestilo_id,dbotalla_id,dbogenero_id)  VALUES(?,?,?,?,?,?,?,?,?,?,?,?)',
@@ -78,19 +78,19 @@ zapatoController.update = async (req, res) => {
         const { id } = req.params;
         const zapato = new Zapato();
         if (typeof req.file !== 'undefined') {
-            zapato.setFoto = req.file.originalname;  //NOMBRE DE LA FOTO DE LA IMG
-            zapato.setMimetype = req.file.mimetype;      //TIPO DE FOTO O IMG
-            zapato.setFilename = req.file.filename;      //NOMBRE ENCRYPTADO DE LA IMG
-            zapato.setPath = 'shoes/' + req.file.filename;//CUANDO SE RECORRA LAS IMG BUSCARA ESTA RUTA DEL "PATH"
-            zapato.setSize = req.file.size;          //VALOR REQ.FILE :tamano de la img : 345kv
-            zapato.setPrecio = req.body.precio;        //CAJA DE TEXTO
-            zapato.setColor = req.body.color;         //CAJA DE TEXTO
-            zapato.setCantidad = req.body.cantidad;      //CAJA DE TEXTO
+            zapato.setFoto      = req.file.originalname;  //NOMBRE DE LA FOTO DE LA IMG
+            zapato.setMimetype  = req.file.mimetype;      //TIPO DE FOTO O IMG
+            zapato.setFilename  = req.file.filename;      //NOMBRE ENCRYPTADO DE LA IMG
+            zapato.setPath      = 'shoes/' + req.file.filename;//CUANDO SE RECORRA LAS IMG BUSCARA ESTA RUTA DEL "PATH"
+            zapato.setSize      = req.file.size;          //VALOR REQ.FILE :tamano de la img : 345kv
+            zapato.setPrecio    = req.body.precio;        //CAJA DE TEXTO
+            zapato.setColor     = req.body.color;         //CAJA DE TEXTO
+            zapato.setCantidad  = req.body.cantidad;      //CAJA DE TEXTO
             zapato.setDBOEstilo = req.body.cboEstilo;     //CAJA DE TEXTO
-            zapato.setDBOTalla = req.body.cboTalla;      //CAJA DE TEXTO
+            zapato.setDBOTalla  = req.body.cboTalla;      //CAJA DE TEXTO
             zapato.setDBOGenero = req.body.cboGenero;     //CAJA DE TEXTO
             zapato.setIdUsuario = req.user.id;            //ID DEL USUARIO LOGEADO
-            zapato.setIdZapato = id;                     //ID DEL ZAPATO UPDATE
+            zapato.setIdZapato  = id;                     //ID DEL ZAPATO UPDATE
             const correct = await pool.query('UPDATE dbozapato SET foto=? , mimetype=? ,filename=? , path=? ,size=? ,precio=? ,color=? ,cantidad=? ,user_id=? ,dboestilo_id=? ,dbotalla_id=? ,dbogenero_id=? WHERE id=?',
                 [zapato.getFoto, zapato.getMimetype, zapato.getFilename, zapato.getPath, zapato.getSize, zapato.getPrecio, zapato.getColor,
                 zapato.getCantidad, zapato.getIdUsuario, zapato.getDBOEstilo, zapato.getDBOTalla, zapato.getDBOGenero, zapato.getIdZapato]);
@@ -102,19 +102,19 @@ zapatoController.update = async (req, res) => {
         } else if (typeof req.file === 'undefined') {
             const dataUpdate = await pool.query('SELECT * FROM dbozapato WHERE id=?', [id]);
             console.log('DATA ZAPATO UPDATE BY ID : ', dataUpdate);
-            zapato.setFoto = dataUpdate[0].foto;      //NOMBRE DE LA FOTO DE LA IMG DE LA BD
-            zapato.setMimetype = dataUpdate[0].mimetype;  //TIPO DE FOTO O IMG DE LA BD
-            zapato.setFilename = dataUpdate[0].filename;  //NOMBRE ENCRYPTADO DE LA IMG DE LA BD
-            zapato.setPath = req.body.foto_actual;    //USAMOS LA RUTA ORIGINAL SI NO SE ACTUALIZA
-            zapato.setSize = dataUpdate[0].size;      //CAJA DE TEXTO
-            zapato.setPrecio = req.body.precio;         //CAJA DE TEXTO
-            zapato.setColor = req.body.color;          //CAJA DE TEXTO
-            zapato.setCantidad = req.body.cantidad;       //CAJA DE TEXTO
+            zapato.setFoto      = dataUpdate[0].foto;      //NOMBRE DE LA FOTO DE LA IMG DE LA BD
+            zapato.setMimetype  = dataUpdate[0].mimetype;  //TIPO DE FOTO O IMG DE LA BD
+            zapato.setFilename  = dataUpdate[0].filename;  //NOMBRE ENCRYPTADO DE LA IMG DE LA BD
+            zapato.setPath      = req.body.foto_actual;    //USAMOS LA RUTA ORIGINAL SI NO SE ACTUALIZA
+            zapato.setSize      = dataUpdate[0].size;      //CAJA DE TEXTO
+            zapato.setPrecio    = req.body.precio;         //CAJA DE TEXTO
+            zapato.setColor     = req.body.color;          //CAJA DE TEXTO
+            zapato.setCantidad  = req.body.cantidad;       //CAJA DE TEXTO
             zapato.setDBOEstilo = req.body.cboEstilo;      //CAJA DE TEXTO
-            zapato.setDBOTalla = req.body.cboTalla;       //CAJA DE TEXTO
+            zapato.setDBOTalla  = req.body.cboTalla;       //CAJA DE TEXTO
             zapato.setDBOGenero = req.body.cboGenero;      //CAJA DE TEXTO
             zapato.setIdUsuario = req.user.id;             //ID DEL USUARIO LOGEADO
-            zapato.setIdZapato = id;                      //ID DEL ZAPATO UPDATE
+            zapato.setIdZapato  = id;                      //ID DEL ZAPATO UPDATE
             const correct = await pool.query('UPDATE dbozapato SET foto=? , mimetype=? ,filename=? , path=? ,size=? ,precio=? ,color=? ,cantidad=? ,user_id=? ,dboestilo_id=? ,dbotalla_id=? ,dbogenero_id=? WHERE id=?',
                 [zapato.getFoto, zapato.getMimetype, zapato.getFilename, zapato.getPath, zapato.getSize, zapato.getPrecio, zapato.getColor,
                 zapato.getCantidad, zapato.getIdUsuario, zapato.getDBOEstilo, zapato.getDBOTalla, zapato.getDBOGenero, zapato.getIdZapato]);
@@ -143,7 +143,7 @@ zapatoController.list = async (req, res) => {
 zapatoController.delete = async (req, res) => {
     try {
         console.log('DATA REQ DELETE: ', req.body.imgDelete);
-        const { id } = req.params;
+        const { id }  = req.params;
         const correct = await pool.query('DELETE FROM dbozapato WHERE id =?', [id]);
         if (correct) {
             await unlink(pathDelete.resolve('./src/public/shoes/' + req.body.imgDelete));

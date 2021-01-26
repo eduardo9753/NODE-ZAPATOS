ad8c7917b906576fd731581f3a321dd0e82d52fd
Eduardo <picis_9716@hotmail.com>
2021-01-26 12:13:15 -0500

    Actualizacion zapatos.controller

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,25 +1,38 @@
-const indexController  = {}
+const indexController = {}
 
 //DATABASE
 const pool = require('../database/database');
 
 //RENDER VIEWS
-indexController.index = (req , res) =>{
+indexController.index = (req, res) => {
     res.render('index.hbs');
 }
+
+
 indexController.shoes = async (req, res) => {
-    const dataShoes = await pool.query('SELECT * FROM dbozapato');
-    console.log('data ZAPATO shoes:' , dataShoes);
-    res.render('shoes.hbs', { dataShoes :dataShoes });
-}
-indexController.shoeView = async (req, res) => {
-    const { id } = req.params;
-    console.log('id view shoe: ', id);
-    const dataViewShoeId = await pool.query('SELECT * FROM dbozapato WHERE id=?' , [id]);
-    res.render('shoesView.hbs', { dataViewShoeId : dataViewShoeId });
+    try {
+        const dataShoes = await pool.query('SELECT * FROM dbozapato');
+        console.log('data ZAPATO shoes:', dataShoes);
+        res.render('shoes.hbs', { dataShoes: dataShoes });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-indexController.map = (req , res) => {
+
+indexController.shoeView = async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log('id view shoe: ', id);
+        const dataViewShoeId = await pool.query('SELECT * FROM dbozapato WHERE id=?', [id]);
+        res.render('shoesView.hbs', { dataViewShoeId: dataViewShoeId });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+indexController.map = (req, res) => {
     res.render('mapa.hbs');
 }
 
